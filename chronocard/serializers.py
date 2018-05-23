03b2f1b4fd85a20ef20d9217fd18cb59374dd11f
Michael V. Battista <github@mvbattista.com>
2018-05-23 16:56:41 -0400

    First view, serializer, URL

---
+++ chronocard/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Event
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ('name', 'start_date', 'end_date', 'work_start_date', 'work_end_date', )
+
--- chronocard/views.py
+++ chronocard/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import authentication, permissions, viewsets
+
+from .models import Event
+from .serializers import EventSerializer
 
 # Create your views here.
+
+class DefaultsMixin(object):
+    # Default settings for views
+
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    )
+
+    permission_classes = (
+        permissions.IsAuthenticated,
+        # permissions.IsAdminUser,
+    )
+
+    paginate_by = 25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+
+class EventViewSet(DefaultsMixin, viewsets.ModelViewSet):
+    queryset = Event.objects.order_by('start_date')
+    serializer_class = EventSerializer
