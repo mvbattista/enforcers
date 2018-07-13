9c78b9d9a1d4169dcb144dace2bfcacb9c6c04c7
Michael V. Battista <github@mvbattista.com>
2018-07-13 16:33:34 -0400

    Stuck on 401s at least

--- chronocard/tests.py
+++ chronocard/tests.py
@@ -1,28 +1,29 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import Group
 from django.urls import reverse, include, path
-from rest_framework.test import APITestCase, APIRequestFactory, URLPatternsTestCase
+from rest_framework.test import APITestCase, APIRequestFactory, APIClient
 from rest_framework import status
 
 from chronocard.models import User
 
 # Create your tests here.
 
-class ChronocardTest(APITestCase, URLPatternsTestCase):
-    urlpatterns = [
-        path('api/', include('enforcers.urls')),
-    ]
+class ChronocardTest(APITestCase):
 
     def setUp(self):
         self.factory = APIRequestFactory()
         self.regular_user = User.objects.create_user(username='test_user', email='test_user@testpass.com',
-                                              password='test_pass', handle='TestUser')
+                                              password='testpass', handle='TestUser')
         self.deputy_user = User.objects.create_user(username='test_deputy', email='test_deputy@testpass.com',
-                                              password='test_deputy', handle='TestDeputy')
+                                              password='testdeputypass', handle='TestDeputy')
         deputy_group = Group.objects.get(name='deputies')
         self.deputy_user.groups.add(deputy_group)
+        self.user_client = APIClient()
+        self.user_client.login(username=self.regular_user.username, password=self.regular_user.password)
+
 
     def test_something(self):
-        url = reverse('user')
-        response = self.client.get(url, format='json')
+        url = '/api/user'
+        response = self.user_client.get(url)
+        print(response.body)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
