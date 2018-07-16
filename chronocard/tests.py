281634d482c90e5018de7855a2d336bf422b4741
Michael V. Battista <github@mvbattista.com>
2018-07-16 17:02:07 -0400

    Test authentication now set up.

--- chronocard/tests.py
+++ chronocard/tests.py
@@ -8,22 +8,35 @@ from chronocard.models import User
 
 # Create your tests here.
 
-class ChronocardTest(APITestCase):
+class ChronocardTestCase(APITestCase):
 
     def setUp(self):
         self.factory = APIRequestFactory()
         self.regular_user = User.objects.create_user(username='test_user', email='test_user@testpass.com',
-                                              password='testpass', handle='TestUser')
+                                                        password='testpass', handle='TestUser')
         self.deputy_user = User.objects.create_user(username='test_deputy', email='test_deputy@testpass.com',
-                                              password='testdeputypass', handle='TestDeputy')
+                                                       password='testdeputypass', handle='TestDeputy')
+        self.admin_user = User.objects.create_superuser(username='test_admin', email='test_admin@testpass.com',
+                                                        password='testpass', handle='TestAdmin')
         deputy_group = Group.objects.get(name='deputies')
         self.deputy_user.groups.add(deputy_group)
         self.user_client = APIClient()
-        self.user_client.login(username=self.regular_user.username, password=self.regular_user.password)
+        self.user_client.force_authenticate(self.regular_user)
+        self.deputy_client = APIClient()
+        self.deputy_client.force_authenticate(self.deputy_user)
+        self.admin_client = APIClient()
+        self.admin_client.force_authenticate(self.admin_user)
 
-
-    def test_something(self):
+    def test_users(self):
         url = '/api/user'
         response = self.user_client.get(url)
-        print(response.body)
+        # print(response.body)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print('Users')
+
+    def test_event_users(self):
+        url = '/api/event_user'
+        response = self.user_client.get(url)
+        # print(response.body)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print('Event Users')
