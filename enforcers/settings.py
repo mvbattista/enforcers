668b9e546fa7ff3802aee2e387d478b8aa53faa2
Michael V. Battista <github@mvbattista.com>
2018-08-16 13:32:15 -0400

    Prepare for Heroku deployment

--- enforcers/settings.py
+++ enforcers/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -154,3 +155,5 @@ SUIT_CONFIG = {
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
